60fd2cb086152730e5e8894cc2ca1f817d1f752b
Hiago 7k <hiago7k@gmail.com>
2026-03-17 20:17:21 -0300

    criando propriedades juntamente com anotacoes

--- Modelos/Pais.cs
+++ Modelos/Pais.cs
@@ -1,5 +1,19 @@
-﻿namespace DesafioApi2.Modelos;
+﻿using System.Text.Json.Serialization;
+
+namespace DesafioApi2.Modelos;
 
 internal class Pais
 {
+    [JsonPropertyName("nome")]
+    public string? Nome { get; set; }
+
+    [JsonPropertyName("capital")]
+    public string? Capital { get; set; }
+
+    [JsonPropertyName("populacao")]
+    public int Populacao { get; set; }
+    [JsonPropertyName("continente")]
+    public string? Continente { get; set; }
+    [JsonPropertyName("idioma")]
+    public string? Idioma { get; set; }
 }

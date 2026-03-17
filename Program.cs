2494322abdf13e71403e7ec29cb4f7919a6c6e57
Hiago 7k <hiago7k@gmail.com>
2026-03-17 20:13:08 -0300

    req incicial da pit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿using (HttpClient httpClient = new HttpClient()) 
+{
+    string resposta = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Paises.json");
+}

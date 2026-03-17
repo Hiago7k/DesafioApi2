49d40dfb0db8a10b429f55d79535573f910a0be1
Hiago 7k <hiago7k@gmail.com>
2026-03-17 20:14:11 -0300

    tratando API try e catch

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using (HttpClient httpClient = new HttpClient()) 
 {
-    string resposta = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Paises.json");
+    try {
+        string resposta = await httpClient.GetStringAsync("https://raw.githubusercontent.com/ArthurOcFernandes/Exerc-cios-C-/curso-4-aula-2/Jsons/Paises.json");
+        Console.WriteLine(resposta);
+    } catch (Exception ex) 
+    {
+        Console.WriteLine($"Erro na busca da api {ex:Message}");
+    }
 }
